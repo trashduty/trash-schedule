5f1bcef8e67b5307b4030b927630a57d2626a816
Joseph Hefner <Hefnerjoseph87@gmail.com>
2025-08-26 15:33:07 -0700

    Update test_cfbfastr.R

--- CFB_Odds/R/test_cfbfastr.R
+++ CFB_Odds/R/test_cfbfastr.R
@@ -10,8 +10,16 @@ library(lubridate)
 library(stringr)
 library(cfbfastR)
 
-cfb_fastR <- cfbfastR::espn_cfb_schedule(year = 2025) |> 
-    filter(type == "regular") |> 
-    mutate(game_date = as_date(with_tz(ymd_hm(game_date), "America/New_York")))
-  
-readr::write_csv(cfb_fastR, "CFB_Odds/Data/cfb_schedule.csv")
+cfb_fastR <- cfbfastR::espn_cfb_schedule(year = 2025)
+
+# Debug: Check what game_date looks like
+print("Raw game_date column:")
+print(head(cfb_fastR$game_date))
+print("Class of game_date:")
+print(class(cfb_fastR$game_date))
+
+cfb_fastR_clean <- cfb_fastR |> 
+  filter(type == "regular") |> 
+  mutate(game_date = as_date(with_tz(ymd_hm(game_date), "America/New_York")))
+
+readr::write_csv(cfb_fastR_clean, "CFB_Odds/Data/cfb_schedule.csv")
