4c572c86b7ce728cdda83bfc558c167be987e63c
Joseph Hefner <Hefnerjoseph87@gmail.com>
2025-08-26 15:14:56 -0700

    Update test_cfbfastr.R

--- CFB_Odds/R/test_cfbfastr.R
+++ CFB_Odds/R/test_cfbfastr.R
@@ -1,3 +1,5 @@
-cfb_fastR <- cfbfastR::espn_cfb_schedule(year = 2025)
+cfb_fastR <- cfbfastR::espn_cfb_schedule(year = 2025) |> 
+    filter(type == "regular") |> 
+    mutate(game_date = as_date(with_tz(ymd_hm(game_date), "America/New_York")))
   
 readr::write_csv(cfb_fastR, "CFB_Odds/Data/cfb_schedule.csv")
